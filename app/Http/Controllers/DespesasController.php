8e51f942c68ca06640586c2e04b9d4a766187896
MatheusZanon <mzblannes@outlook.com>
2021-08-22 23:31:21 -0300

    Configurando o envio de emails e finalizando projeto

--- app/Http/Controllers/DespesasController.php
+++ app/Http/Controllers/DespesasController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\DespesaCadastrada;
 use App\Models\Despesas;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class DespesasController extends Controller
 {
@@ -21,12 +24,15 @@ class DespesasController extends Controller
     public function store(Request $request)
     {
         $userId = Auth::user()->id;
+        $user = User::where('id', '=', $userId)->first();
 
         $request->validate([
             'data' => 'required|date|before:tomorrow',
             'valor' => 'required|numeric|min:0',
             'descricao' => 'required|max:191'
         ]);
+
+        Mail::to($user->email)->later(now()->addMinutes(10), new DespesaCadastrada());
 
         return Despesas::create([
             'user_id' => $userId,
---
+++ app/Mail/DespesaCadastrada.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class DespesaCadastrada extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('mzblannes@outlook.com', 'Matheus Zanon')->subject('Cadastro de Despesa')->view('emails.cadastrado');
+    }
+}
